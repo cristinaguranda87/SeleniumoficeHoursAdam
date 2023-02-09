eaafb5b182ade834cf970cebd1b8d9690350fbaf
Cristina_Guranda <cbalanuta@gmail.com>
2023-02-08 19:12:13 -0500

    i added new base pakage

---
+++ src/test/java/com/cydeo/tests/base/VyTrackBase.java
@@ -0,0 +1,29 @@
+package com.cydeo.tests.base;
+
+import com.cydeo.tests.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class VyTrackBase {
+ protected   WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+// 2. Navigate to url 'http://automationexercise.com'
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://vytrack.com/");
+
+
+
+
+    }
+   @AfterMethod
+    public void tearDown(){
+driver.close();
+   }
+}
