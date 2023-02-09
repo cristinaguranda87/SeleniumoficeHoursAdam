6b05dd5daf88a7617eb8149a1744777b23448540
Cristina_Guranda <cbalanuta@gmail.com>
2023-02-08 19:11:22 -0500

    i added new package week03

---
+++ src/test/java/com/cydeo/tests/week03/T1.java
@@ -0,0 +1,18 @@
+package com.cydeo.tests.week03;
+
+import com.cydeo.tests.base.VyTrackBase;
+
+public class T1 extends VyTrackBase {
+    /*
+TC 1
+1- As a user I should be able to see the
+labels [“Home”,”About us”,”Our Approach”,”Products and Services”,”Contact”,”LOGIN”]
+1- Open a chrome browser
+2- Go to "https://vytrack.com/"
+3- Verify that ["Home","About us","Our Approach",
+"Products and Services","Contact","LOGIN"] is displayed
+     */
+
+
+
+}
